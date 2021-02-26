21718e27927874fcf0a919cab3e8fb87cdf4c9c1
Tanmay Sharma <tanmaysharma19@gmail.com>
2021-02-26 10:32:18 -0800

    Add a function for standard scaling of numeric features

---
+++ pymleda/pymleda.py
@@ -0,0 +1,19 @@
+def dfscaling(df):
+    """
+    Apply standard scaling to the numeric features of a given dataframe. 
+    Parameters
+    ----------
+    df : pandas.DataFrame
+    	A pandas data frame.
+    Returns
+    -------
+    scaled_df : pandas.DataFrame
+      A data frame with standard scaling applied to the numeric features.  
+    Examples
+    --------
+    >>> from pymleda import pymleda
+    >>> df = pd.read_csv("test_data.csv")
+    >>> dfscaling(df)
+    """ 
+
+    return scaled_df
