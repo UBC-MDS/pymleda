25d2f1c5a175bdb63f4e2bfb078d7121f0ede590
salehyaz <yazan.saleh@alum.utoronto.ca>
2021-03-04 21:43:01 -0500

    Write unit test for data splitting

--- tests/test_SupervisedData.py
+++ tests/test_SupervisedData.py
@@ -9,4 +9,22 @@ def test_supervised_data_original_dataset():
         {"col1": [1, 1, 1, 1], "col2": [2, 2, 2, 2], "col3": [3, 3, 3, 3]}
     )
 
-    assert pymleda.SupervisedData(toy_data).data == toy_data
+    assert (
+        pymleda.SupervisedData(toy_data, x_cols=["col1", "col2"], y_cols=["col3"]).data
+        == toy_data
+    )
+
+
+def test_supervised_data_split_sizes():
+    # Test that the data splits are of correct sizes
+
+    toy_data = pd.DataFrame(
+        {"col1": [1, 1, 1, 1], "col2": [2, 2, 2, 2], "col3": [3, 3, 3, 3]}
+    )
+
+    supervised_data = pymleda.SupervisedData(
+        toy_data, x_cols=["col1", "col2"], y_cols=["col3"]
+    )
+
+    assert len(supervised_data.test_df) == 0.75 * len(toy_data)
+    assert len(supervised_data.train_df) == 0.25 * len(toy_data)
