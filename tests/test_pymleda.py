3880a1d56c07e30bad5ab4843297dc38914102b3
Saule Atymtayeva <sa080388@student.ubc.ca>
2021-03-05 09:39:14 -0800

    Update pytest.fixture for autoimpute_na()

--- tests/test_pymleda.py
+++ tests/test_pymleda.py
@@ -5,7 +5,7 @@ from pandas.util.testing import assert_frame_equal
 from pymleda import pymleda
 
 @pytest.fixture
-def input_value():
+def model_df():
     """Create the model dataframe with no missing values"""
     choco_data = {'Chocolate_brand': ['Lindt','Rakhat','Lindt','Richart', 'Lindt'],
                   'Price': [3.0, 4.0, 4.0, 6.0, 3.0]}
@@ -22,13 +22,13 @@ choco_data_2 = {'Chocolate_brand': ['Lindt','Rakhat','Lindt','Richart', 'not ava
 toy_df_2 = pd.DataFrame(choco_data_2, columns = ['Chocolate_brand', 'Price'])
 
 
-def test_autoimpute_na_1(input_value):
+def test_autoimpute_na_1(model_df):
     """Test that the output dataframe of the pymleda.autoimpute_na() function
     with no missing values is equal to the model dataframe (identical to the original one)"""
-    assert_frame_equal(pymleda.autoimpute_na(toy_df_1), input_value)
+    assert_frame_equal(pymleda.autoimpute_na(toy_df_1), model_df)
 
 
-def test_autoimpute_na_2(input_value):
+def test_autoimpute_na_2(model_df):
     """Test that the output dataframe of the pymleda.autoimpute_na() function
     with some missing values is equal to the model dataframe"""
-    assert_frame_equal(pymleda.autoimpute_na(toy_df_2), input_value)
+    assert_frame_equal(pymleda.autoimpute_na(toy_df_2), model_df)
