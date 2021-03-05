bc656b4ff57a119e368dff8e047ff02a9f17409d
rissangs <70673560+rissangs@users.noreply.github.com>
2021-03-05 08:30:23 -0800

    Add code of test_dftype function

--- tests/test_pymleda.py
+++ tests/test_pymleda.py
@@ -1,5 +1,30 @@
 from pymleda import __version__
 from pymleda import pymleda
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_dftype():
+    
+    df = pd.DataFrame({
+        'type':['Air','Ship','Bus', 'Air' ],
+         'time':[6,32,31,5],
+         'origin': ['US', 'Mexico', 'CANADA', 'UK']
+    })
+    
+    
+    assert type(df) == (pd.core.frame.DataFrame), \
+        "The date type of input should be data frame."
+    
+    assert type(pymleda.dftype(df)[0]) == (pd.core.frame.DataFrame), \
+        "The date type of summry should be data frame."    
+   
+    assert type(pymleda.dftype(df)[1]) == (pd.core.frame.DataFrame), \
+         "The date type of unique_val should be data frame."
+    
+    assert len(pymleda.dftype(df)[0]) == 8, \
+         "The length of summary data frame is incorrect."
+    
+    assert list(pymleda.dftype(df)[1].unique_values[0]) == ['US', 'Mexico', 'CANADA', 'UK'], \
+         "The unique values are incorrect."
